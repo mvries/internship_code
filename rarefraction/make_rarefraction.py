97a5826adfb1b506c8c286cf89fa1aa403a3e5a4
m97devries <m97devries@gmail.com>
2024-02-13 19:02:44 +0100

    final comit for day

--- rarefraction/make_rarefraction.py
+++ rarefraction/make_rarefraction.py
@@ -11,36 +11,36 @@ def quality_check():
     This function performs a quality check of the input reads:
     """
 
-    cmd = "fastp -i " + fastq1 + " -I " + fastq2 + " -o " + output_dir + "tmp/" \
-    + "trimmed_1.fq" + " -O " + output_dir + "tmp/" + "trimmed_2.fq"
+    cmd = "fastp -f 20 -F 20 -i " + fastq1 + " -I " + fastq2 + " -o " + output_dir + "/tmp/" \
+    + "trimmed_1.fq" + " -O " + output_dir + "/tmp/" + "trimmed_2.fq"
 
     subprocess.check_call(cmd, shell=True)
 
-    trimmed_1 = output_dir + "tmp/" + "trimmed_1.fq"
-    trimmed_2 = output_dir + "tmp/" + "trimmed_2.fq"
+    trimmed_1 = output_dir + "/tmp/" + "trimmed_1.fq"
+    trimmed_2 = output_dir + "/tmp/" + "trimmed_2.fq"
 
     return trimmed_1, trimmed_2
 
 def sample_reads(trimmed_1, trimmed_2):
     """
-    This function makes fractional samples of reads
+    This function makes samples of reads
     """
 
-    fractions = [i for i in range(1,10)]
+    fractions = [i for i in range(10000,100000, 10000)]
 
     for fraction in fractions:
 
         try:
-            cmd = "mkdir " + output_dir + "tmp/" + "fraction_" + str(fraction)
+            cmd = "mkdir " + output_dir + "/tmp/" + "fraction_" + str(fraction)
             subprocess.check_call(cmd, shell=True)
         except:
             pass
 
-        cmd = "seqtk sample -s100 " + trimmed_1 + " 0." + str(fraction) + " > " +\
+        cmd = "seqtk sample -s100 " + trimmed_1 + " " + str(fraction) + " > " +\
         output_dir + "tmp/fraction_" + str(fraction) + "/file1.fq"
         subprocess.check_call(cmd, shell=True)
 
-        cmd = "seqtk sample -s100 " + trimmed_2 + " 0." + str(fraction) + " > " +\
+        cmd = "seqtk sample -s100 " + trimmed_2 + " " + str(fraction) + " > " +\
         output_dir + "tmp/fraction_" + str(fraction) + "/file2.fq"
         subprocess.check_call(cmd, shell=True)
 
@@ -49,15 +49,12 @@ def merge_reads():
     This function assembles/merges the paired end reads
     """
 
-    fractions = [i for i in range(1,10)]
+    fractions = [i for i in range(10000,100000, 10000)]
 
     for fraction in fractions:
-        cmd = "flash -M 250 -d " + output_dir + "tmp/fraction_" + str(fraction) + "/ "\
+        cmd = "flash -M 250 -m 50 -d " + output_dir + "/tmp/fraction_" + str(fraction) + "/ "\
         + output_dir + "tmp/fraction_" + str(fraction) + "/file1.fq "\
         + output_dir + "tmp/fraction_" + str(fraction) + "/file2.fq "
-
-        print(cmd)
-
         subprocess.check_call(cmd, shell=True)
 
 
@@ -66,7 +63,7 @@ def main():
     print("Making rarefraction curve!")
 
     try:
-        cmd = "mkdir " + output_dir + "tmp/"
+        cmd = "mkdir " + output_dir + "tmp"
         subprocess.check_call(cmd, shell=True)
     except:
         pass
